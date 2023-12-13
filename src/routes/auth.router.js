69dcde5243a6bb026cc0ef26459bde8964556844
김영우 <youngwookim@gim-yeong-uui-MacBookPro.local>
2023-12-13 11:27:46 +0900

    feat: 일단 auth 저장

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -2,3 +2,62 @@ import express from "express";
 const router = express.Router();
 
 export default router;
+
+import { Router } from "express";
+
+const authRouter = Router();
+
+// 회원가입
+authRouter.post("/signup", async (req, res) => {});
+
+// 로그인
+authRouter.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({
+        success: false,
+        message: "이메일 입력이 필요합니다."
+      });
+    }
+
+    if (!password) {
+      return res.status(400).json({
+        success: false,
+        message: "비밀번호 입력이 필요합니다."
+      });
+    }
+
+    const user = (await Users.findOne({ where: { email } }))?.toJSON();
+    const hashedPassword = user?.password ?? "";
+    const isPasswordMatched = bcrypt.compareSync(password, hashedPassword);
+
+    const isCorrectUser = user && isPasswordMatched;
+
+    if (!isCorrectUser) {
+      return res.status(401).json({
+        success: false,
+        message: "일치하는 인증 정보가 없습니다."
+      });
+    }
+
+    const accessToken = jwt.sign({ userId: user.id }, JWT_ACCESS_TOKEN_SECRET, {
+      expiresIn: JWT_ACCESS_TOKEN_EXPIRES_IN
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "로그인에 성공했습니다.",
+      data: { accessToken }
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "예상치 못한 에러가 발생하였습니다. 관리자에게 문의하세요."
+    });
+  }
+});
+
+export { authRouter };
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,3 +1,38 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import db from "../../models/index.cjs";
+import {
+  PASSWORD_HASH_SALT_ROUNDS,
+  JWT_ACCESS_TOKEN_SECRET,
+  JWT_ACCESS_TOKEN_EXPIRES_IN
+} from "../constants/security.costant.js";
+import * as HttpStatus from "../errors/http-status.error.js";
 import { UsersRepository } from "../repositories/users.repository.js";
+const { Users } = db;
 
-export class AuthService {}
+export class AuthService {
+  constructor() {
+    this.usersRepository = new UsersRepository();
+  }
+  signup = async ({ email, name, password }) => {
+    const existedUser = await this.usersRepository.readOneByEmail(email);
+
+    if (existedUser) {
+      throw new HttpStatus.BadRequest("이미 가입 된 이메일");
+    }
+
+    // const hashedPassword = bcrypt.hashSync(password, PASSWORD_HASH_SALT_ROUNDS);
+
+    const newUser = await this.usersRepository.createOne({
+      email,
+      password,
+      name
+    });
+    // (
+    //   await Users.create({ email, password: hashedPassword, name })
+    // ).toJSON();
+    // delete newUser.password;
+
+    return newUser;
+  };
+}
