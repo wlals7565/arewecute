81c3de7850acca3ce949aa27bdb1b05e32de3dcc
김영우 <youngwookim@gim-yeong-uui-MacBookPro.local>
2023-12-13 23:56:32 +0900

    fix: 서버 터짐 복구

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { reservationsRouter } from "./reservations.router.js";
+import usersRouter from "./users.router.js";
 const router = express.Router();
 
 router.use("/reservation", reservationsRouter);
+router.use("/users/", usersRouter);
 
 export default router;
