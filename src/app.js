90c473d350dfafb56a614198ce49310e144606c4
wlals7565 <117993640+wlals7565@users.noreply.github.com>
2023-12-14 15:19:52 +0900

    FIX BUG

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const {sequelize} = db
 const app = express();
 const port = 3000;
 
-sequelize.sync({ force: true})
+sequelize.sync({ force: false})
 .then(()=> {
     console.log(`데이터베이스 연결 성공`);
 })
