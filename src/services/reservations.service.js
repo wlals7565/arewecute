b967df2289d08cd5f182817e784d29ab725b42e5
asdfg20564 <44521546+asdfg20564@users.noreply.github.com>
2023-12-14 19:45:08 +0900

    add deleteOne
    add deleteOne

--- src/services/reservations.service.js
+++ src/services/reservations.service.js
@@ -1,8 +1,56 @@
 import { ReservationsRepository } from "../repositories/reservations.repository.js";
+import db from "../../models/index.cjs";
+const { reservations } = db;
 
 export class ReservationsService {
   constructor() {
     this.reservationsRepository = new ReservationsRepository();
   }
-  readOne = async ({ id }) => {};
+
+  readById = async ({ id }) => {
+    const reservation = await reservations.findByPk(id);
+
+    return reservation;
+  };
+
+  isReserved = async ({ petSitterId, reservedAt }) => {
+    const prereservation = await reservations.findAll({
+      where: {
+        petSitterId,
+        reservedAt
+      }
+    });
+
+    return prereservation.length ? true : false;
+  };
+
+  createdOne = async ({ userId, petSitterId, company, comment, animal, reservedAt }) => {
+    const data = await reservations.create({
+      userId,
+      petSitterId,
+      company,
+      comment,
+      animal,
+      reservedAt
+    });
+
+    return data;
+  };
+
+  updateOne = async ({ id, petSitterId, company, comment, animal, reservedAt }) => {
+    await reservations.update(
+      {
+        petSitterId,
+        company,
+        animal,
+        reservedAt,
+        comment
+      },
+      { where: { id: id } }
+    );
+  };
+
+  deleteOne = async ({ id }) => {
+    await reservations.destroy({ where: { id: id } });
+  };
 }
