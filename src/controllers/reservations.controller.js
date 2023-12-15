f7b4e3d768930abdf9174dfeeaf68060d081b091
asdfg20564 <44521546+asdfg20564@users.noreply.github.com>
2023-12-15 15:59:45 +0900

    update auth middleware
    update auth middleware

--- src/controllers/reservations.controller.js
+++ src/controllers/reservations.controller.js
@@ -4,39 +4,29 @@ export class ReservationsController {
   constructor() {
     this.reservationService = new ReservationsService();
   }
+
   createOne = async (req, res, next) => {
     try {
       if (!req.body.petSitterId || !req.body.company || !req.body.comment || !req.body.animal || !req.body.date) {
-        return res.status(400).json({
-          success: false,
-          errorMessage: "데이터 형식이 올바르지 않습니다."
-        });
+        throw new Error("InvalidParamsError");
       }
 
       const { petSitterId, company, comment, animal, date } = req.body;
-      //const { userId } = res.locals.user;
-      const userId = 1;
+      const userId = res.locals.userId;
 
       //오늘보다 과거에 예약을 하면 안됨
-      const today = new Date();
-      today.setUTCHours(0, 0, 0, 0);
       const reservedAt = new Date(date);
+      const isPastThanToday = await this.reservationService.isPastThanToday({ reservedAt });
 
-      if (reservedAt <= today) {
-        return res.status(400).json({
-          success: false,
-          errorMessage: "오늘 이후의 날짜로 예약해주세요."
-        });
+      if (isPastThanToday) {
+        throw new Error("PastThanToday");
       }
 
       //해당 펫시터의 다른 예약과 날짜 겹치는지 확인
       const prereservation = await this.reservationService.isReserved({ petSitterId, reservedAt });
 
       if (prereservation) {
-        return res.status(400).json({
-          success: false,
-          errorMessage: "해당 펫시터는 그 날 예약이 불가능합니다."
-        });
+        throw new Error("Prereservation");
       }
 
       const data = await this.reservationService.createdOne({
@@ -54,36 +44,25 @@ export class ReservationsController {
         data
       });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-        success: false,
-        message: "예상치 못한 에러가 발생하였습니다. 관리자에게 문의하세요."
-      });
+      next(error);
     }
   };
 
   readOne = async (req, res, next) => {
     try {
       const { reservationId } = req.params;
-      //const { userId } = res.locals.user;
-      const userId = 1;
+      const userId = res.locals.userId;
 
-      const data = await this.reservationService.readById({ id: reservationId });
+      const data = await this.reservationService.readByIdWithUser({ id: reservationId });
 
       if (!data) {
-        return res.status(404).json({
-          success: false,
-          message: "해당 예약을 찾을 수 없습니다."
-        });
+        throw new Error("NoReservation");
       }
 
       //userId랑 다르면 읽기 불가
       const ownerReservation = data.userId === userId;
       if (!ownerReservation) {
-        return res.status(403).json({
-          success: false,
-          message: "예약 조회에 권한이 없습니다."
-        });
+        throw new Error("ownerReservation");
       }
 
       return res.status(200).json({
@@ -92,11 +71,23 @@ export class ReservationsController {
         data
       });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-        success: false,
-        message: "예상치 못한 에러가 발생하였습니다. 관리자에게 문의하세요."
+      next(error);
+    }
+  };
+
+  readByUser = async (req, res, next) => {
+    try {
+      const userId = res.locals.userId;
+
+      const data = await this.reservationService.readByUser({ userId });
+
+      return res.status(200).json({
+        success: true,
+        message: "예약 조회에 성공했습니다.",
+        data
       });
+    } catch (error) {
+      next(error);
     }
   };
 
@@ -104,66 +95,51 @@ export class ReservationsController {
     try {
       const { reservationId } = req.params;
       if (!req.body.petSitterId || !req.body.company || !req.body.comment || !req.body.animal || !req.body.date) {
-        return res.status(400).json({
-          success: false,
-          errorMessage: "데이터 형식이 올바르지 않습니다."
-        });
+        throw new Error("InvalidParamsError");
       }
 
       const { petSitterId, company, comment, animal, date } = req.body;
-      //const { userId } = res.locals.user;
-      const userId = 1;
+      const userId = res.locals.userId;
 
       const data = await this.reservationService.readById({ id: reservationId });
 
       if (!data) {
-        return res.status(404).json({
-          success: false,
-          message: "해당 예약을 찾을 수 없습니다."
-        });
+        throw new Error("NoReservation");
       }
 
       //userId랑 다르면 수정 불가
       const ownerReservation = data.userId === userId;
       if (!ownerReservation) {
-        return res.status(403).json({
-          success: false,
-          message: "예약 수정에 권한이 없습니다."
-        });
+        throw new Error("ownerReservation");
       }
 
-      // 시간이 지났거나 state가 완료인 경우 예약 변경 불가
-      const today = new Date();
-      const reservedAt = new Date(date);
-      today.setUTCHours(0, 0, 0, 0);
+      // 시간이 지났거나 status 완료인 경우 예약 변경 불가
+      const isCompleted = await this.reservationService.isCompleted({
+        ReservedDate: data.reservedAt,
+        Reservedstatus: data.status
+      });
 
-      if (data.reservedAt <= today || data.state === "완료") {
-        return res.status(400).json({
-          success: false,
-          errorMessage: "지난 예약은 수정이 불가능합니다."
-        });
+      if (isCompleted) {
+        throw new Error("CompletedReservation");
       }
 
       //예약 날짜 변경 시
+      const reservedAt = new Date(date);
       const beforeUpdatedReservedAt = data.reservedAt.toISOString().split("T")[0];
 
       if (beforeUpdatedReservedAt !== date) {
         //오늘보다 앞의 날짜에 예약하면 안됨
-        if (reservedAt <= today) {
-          return res.status(400).json({
-            success: false,
-            errorMessage: "오늘 이후의 날짜로 예약해주세요."
-          });
+        const isPastThanToday = await this.reservationService.isPastThanToday({ reservedAt });
+
+        if (isPastThanToday) {
+          throw new Error("PastThanToday");
         }
 
         //해당 펫시터의 다른 예약과 날짜 겹치는지 확인
         const prereservation = await this.reservationService.isReserved({ petSitterId, reservedAt });
 
         if (prereservation) {
-          return res.status(400).json({
-            success: false,
-            errorMessage: "해당 펫시터는 그 날 예약이 불가능합니다."
-          });
+          throw new Error("Prereservation");
         }
       }
 
@@ -172,9 +148,9 @@ export class ReservationsController {
         id: reservationId,
         petSitterId,
         company,
+        comment,
         animal,
-        reservedAt,
-        comment
+        reservedAt
       });
 
       const updatedData = await this.reservationService.readById({ id: reservationId });
@@ -185,47 +161,35 @@ export class ReservationsController {
         updatedData
       });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-        success: false,
-        message: "예상치 못한 에러가 발생하였습니다. 관리자에게 문의하세요."
-      });
+      next(error);
     }
   };
 
   deleteOne = async (req, res, next) => {
     try {
       const { reservationId } = req.params;
-      //const { userId } = res.locals.user;
-      const userId = 1;
+      const userId = res.locals.userId;
 
       const data = await this.reservationService.readById({ id: reservationId });
 
       if (!data) {
-        return res.status(404).json({
-          success: false,
-          message: "해당 예약을 찾을 수 없습니다."
-        });
+        throw new Error("NoReservation");
       }
 
       //userId랑 다르면 삭제 불가
       const ownerReservation = data.userId === userId;
       if (!ownerReservation) {
-        return res.status(403).json({
-          success: false,
-          message: "예약 삭제에 권한이 없습니다."
-        });
+        throw new Error("ownerReservation");
       }
 
-      // 시간이 지났거나 state가 완료인 경우 예약 삭제 불가 (완료한 것은 완료한 채로 넣어져있음)
-      const today = new Date();
-      today.setUTCHours(0, 0, 0, 0);
+      // 시간이 지났거나 status가 완료인 경우 예약 삭제 불가 (완료한 것은 완료한 채로 넣어져있음)
+      const isCompleted = await this.reservationService.isCompleted({
+        ReservedDate: data.reservedAt,
+        Reservedstatus: data.status
+      });
 
-      if (data.reservedAt <= today || data.state === "완료") {
-        return res.status(400).json({
-          success: false,
-          errorMessage: "지난 예약은 수정이 불가능합니다."
-        });
+      if (isCompleted) {
+        throw new Error("CompletedReservation");
       }
 
       await this.reservationService.deleteOne({ id: reservationId });
@@ -236,11 +200,7 @@ export class ReservationsController {
         data
       });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-        success: false,
-        message: "예상치 못한 에러가 발생하였습니다. 관리자에게 문의하세요."
-      });
+      next(error);
     }
   };
 }
